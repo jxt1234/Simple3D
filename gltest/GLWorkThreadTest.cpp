926387f2b662c2a8981e6a73527437a20183d5d6
jxt1234and2006@gmail.com <jxt1234@zju.edu.cn>
2014-10-13 21:13:19 +0800

    Complete GLWorkThreadTest for Linux version

--- gltest/GLWorkThreadTest.cpp
+++ gltest/GLWorkThreadTest.cpp
@@ -5,6 +5,8 @@
 #include "GL/GLWorkThread.h"
 #include <GL/glew.h>
 #include <GL/glut.h>
+#include <X11/Xlib.h>
+#include <GL/glx.h>
 
 class GLWorkThreadTest:public GLTest
 {
@@ -16,9 +18,40 @@ class GLWorkThreadTest:public GLTest
 
 void create()
 {
-    int p=1;
-    glutInit(&p, NULL);
-    glutCreateWindow("Test");
+    GLXContext fContext;
+    Display* fDisplay = XOpenDisplay(0);
+    Pixmap fPixmap;
+    GLXPixmap fGlxPixmap;
+    int fbcount;
+    static int visual_attribs[] = {
+        GLX_X_RENDERABLE    , True,
+        GLX_DRAWABLE_TYPE   , GLX_PIXMAP_BIT,
+        None
+    };
+    GLXFBConfig *fbc = glXChooseFBConfig(fDisplay, DefaultScreen(fDisplay),
+            visual_attribs, &fbcount);
+    int best_fbc = -1, best_num_samp = -1;
+
+    int i;
+    for (i = 0; i < fbcount; ++i) {
+        XVisualInfo *vi = glXGetVisualFromFBConfig(fDisplay, fbc[i]);
+        if (vi) {
+            int samp_buf, samples;
+            glXGetFBConfigAttrib(fDisplay, fbc[i], GLX_SAMPLE_BUFFERS, &samp_buf);
+            glXGetFBConfigAttrib(fDisplay, fbc[i], GLX_SAMPLES, &samples);
+            if (best_fbc < 0 || (samp_buf && samples > best_num_samp))
+                best_fbc = i, best_num_samp = samples;
+        }
+        XFree(vi);
+    }
+    GLXFBConfig bestFbc = fbc[best_fbc];
+    XFree(fbc);
+    XVisualInfo *vi = glXGetVisualFromFBConfig(fDisplay, bestFbc);
+    fPixmap = XCreatePixmap(fDisplay, RootWindow(fDisplay, vi->screen), 10, 10, vi->depth);
+    fGlxPixmap = glXCreateGLXPixmap(fDisplay, vi, fPixmap);
+    XFree(vi);
+    fContext = glXCreateNewContext(fDisplay, bestFbc, GLX_RGBA_TYPE, 0, True);
+    glXMakeCurrent(fDisplay, fGlxPixmap, fContext);
     glewInit();
 }
 void GLWorkThreadTest::run()
@@ -37,4 +70,4 @@ void GLWorkThreadTest::run()
     dst->save("output/GLWorkThreadTest.png");
     worker.stop();
 }
-//static GLTestRegister<GLWorkThreadTest> a("GLWorkThreadTest");
+static GLTestRegister<GLWorkThreadTest> a("GLWorkThreadTest");
